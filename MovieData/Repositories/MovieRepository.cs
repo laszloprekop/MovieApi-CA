9461127353760a2b7cd3aa972e174423fb77bd40
Laszlo <laszlo.prekop@gmail.com>
2026-06-25 16:32:40 +0200

    feat(data): implement basic MovieRepository

---
+++ MovieData/Repositories/MovieRepository.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.EntityFrameworkCore;
+using MovieCore.DomainContracts;
+using MovieCore.Models;
+
+namespace MovieData.Repositories;
+
+public class MovieRepository(MovieContext context) : IMovieRepository
+{
+    public async Task<IEnumerable<Movie>> GetAllAsync() => await context.Movies.ToListAsync();
+
+    public Task<Movie?> GetAsync(int id) => context.Movies.FindAsync(id).AsTask();
+
+    public Task<bool> AnyAsync(int id) => context.Movies.AnyAsync(m => m.Id == id);
+
+    public void Add(Movie movie) => context.Movies.Add(movie);
+    public void Update(Movie movie) => context.Movies.Update(movie);
+    public void Remove(Movie movie) => context.Movies.Remove(movie);
+}
