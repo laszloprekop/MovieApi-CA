6715c87b686a3c442fe4287c80d000b3a0abbb01
Laszlo <laszlo.prekop@gmail.com>
2026-06-25 16:26:24 +0200

    feat(core): add IMovieRepository contract

---
+++ MovieCore/DomainContracts/IMovieRepository.cs
@@ -0,0 +1,13 @@
+﻿namespace MovieCore.DomainContracts;
+
+using MovieCore.Models;
+
+public interface IMovieRepository
+{
+    Task<IEnumerable<Movie>> GetAllAsync();
+    Task<Movie?> GetAsync(int id);
+    Task<bool> AnyAsync(int id);
+    void Add(Movie movie);
+    void Update(Movie movie);
+    void Remove(Movie movie);
+}
