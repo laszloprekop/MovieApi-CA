be3ab0ed72e99bfbced464ab239220bda0b39c0e
Laszlo <laszlo.prekop@gmail.com>
2026-06-26 22:05:16 +0200

    feat(services): implement MovieService

--- MovieServices/MovieService.cs
+++ MovieServices/MovieService.cs
@@ -2,11 +2,29 @@
 using MovieContracts;
 using MovieCore.DomainContracts;
 using MovieCore.DTOs;
+using MovieCore.Exceptions;
+using MovieCore.Models;
 
 namespace MovieServices;
 
 public class MovieService(IUnitOfWork uow, IMapper mapper) : IMovieService
 {
-    public Task<MovieDto> GetAsync(int id) => Task.FromResult(new MovieDto()); // stub
-    public Task<MovieDto> CreateAsync(MovieCreateDto dto) => throw new NotImplementedException();
+    public async Task<MovieDto> GetAsync(int id)
+    {
+        var movie = await uow.Movies.GetAsync(id);
+        if (movie == null)
+        {
+            throw new NotFoundException($"Movie {id} not found");
+        }
+
+        return mapper.Map<MovieDto>(movie);
+    }
+
+    public async Task<MovieDto> CreateAsync(MovieCreateDto dto)
+    {
+        var movie = mapper.Map<Movie>(dto);
+        uow.Movies.Add(movie);
+        await uow.CompleteAsync();
+        return mapper.Map<MovieDto>(movie);
+    }
 }
