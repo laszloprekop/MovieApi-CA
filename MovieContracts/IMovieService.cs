8408c03b54e587de15fcf1dc06f2f7a587d4f9e0
Laszlo <laszlo.prekop@gmail.com>
2026-06-26 22:37:07 +0200

    feat(contracts): extend IMovieService to full Movies surface

--- MovieContracts/IMovieService.cs
+++ MovieContracts/IMovieService.cs
@@ -4,6 +4,10 @@ namespace MovieContracts;
 
 public interface IMovieService
 {
+    Task<IEnumerable<MovieDto>> GetAllAsync(string? genre, int? year, string? actor);
     Task<MovieDto> GetAsync(int id);
+    Task<MovieDetailDto> GetDetailsAsync(int id);
     Task<MovieDto> CreateAsync(MovieCreateDto dto);
+    Task UpdateAsync(int id, MovieUpdateDto dto);
+    Task DeleteAsync(int id);
 }
