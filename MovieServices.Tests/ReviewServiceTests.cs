865d27b23cf23279192a3101de924523850ab3df
Laszlo <laszlo.prekop@gmail.com>
2026-06-28 22:18:39 +0200

    feat(services.test): add tests for review count and test data utilities

---
+++ MovieServices.Tests/ReviewServiceTests.cs
@@ -0,0 +1,20 @@
+﻿using MovieCore.DomainContracts;
+using MovieCore.DTOs;
+using MovieCore.Exceptions;
+using NSubstitute;
+
+namespace MovieServices.Tests;
+
+public class ReviewServiceTests
+{
+    [Fact]
+    public async Task CreateAsync_WhenMovieAlreadyHas10Reviews_Throws()
+    {
+        var uow = Substitute.For<IUnitOfWork>();
+        uow.Movies.GetWithReviewsAsync(1).Returns(TestData.MovieWithReviews(10));
+        var sut = new ReviewService(uow);
+
+        await Assert.ThrowsAsync<BusinessRuleException>(() =>
+            sut.CreateAsync(1, new ReviewDto { ReviewerName = "X", Comment = "c", Rating = 4 }));
+    }
+}
---
+++ MovieServices.Tests/TestData.cs
@@ -0,0 +1,29 @@
+﻿using MovieCore.Models;
+
+namespace MovieServices.Tests;
+
+internal static class TestData
+{
+    public static Movie MovieWithReviews(int count, int? year = null)
+    {
+        var movie = new Movie { Id = 1, Title = "Test Movie", Year = year ?? DateTime.UtcNow.Year };
+        for (var i = 0; i < count; i++)
+        {
+            movie.Reviews.Add(new Review { ReviewerName = $"R{i}", Comment = $"C{i}", Rating = 3 });
+        }
+
+        return movie;
+    }
+
+    public static Movie MovieWithActors(int count, bool documentary)
+    {
+        var movie = new Movie { Id = documentary ? 5 : 1, Title = "Test Movie", Year = 2000 };
+        if (documentary) movie.Genres.Add(new Genre { Name = Genres.Documentary });
+        for (var i = 0; i < count; i++)
+        {
+            movie.Actors.Add(new Actor { Id = i, Name = $"A{i}" });
+        }
+
+        return movie;
+    }
+}
